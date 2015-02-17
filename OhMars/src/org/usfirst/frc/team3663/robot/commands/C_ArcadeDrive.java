f4a425e3f50b04752e4df261be41fb5dc8319716
CurtisAndersen <curtis@loginet.com>
2015-02-17 15:48:09 -0800

    encoders are on their way

--- OhMars/src/org/usfirst/frc/team3663/robot/commands/C_ArcadeDrive.java
+++ OhMars/src/org/usfirst/frc/team3663/robot/commands/C_ArcadeDrive.java
@@ -20,23 +20,17 @@ public class C_ArcadeDrive extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-<<<<<<< HEAD
-    	double Speed;
-    	if(Robot.oi.driveController.getRawButton(4) == false){
-    		Speed = Robot.oi.driveController.getRawAxis(2) - Robot.oi.driveController.getRawAxis(3);
-    		Robot.ssDriveTrain.arcadeDrive(Speed, -Robot.oi.driveController.getRawAxis(0));
-    	}
-    	else{
-    		Speed = Robot.oi.driveController.getRawAxis(2) - Robot.oi.driveController.getRawAxis(3);
-    		Robot.ssDriveTrain.arcadeDrive((Robot.oi.driveController.getRawAxis(1)/2), (-Robot.oi.driveController.getRawAxis(0)/2));
-    		//Robot.ssDriveTrain.arcadeDrive(Robot.oi.driveController.getRawAxis(0), Speed);
-=======
-    	if(Robot.oi.driveControllerOn){
-	    	double Speed = Robot.oi.driveController.getRawAxis(2) - Robot.oi.driveController.getRawAxis(3);
-	    	Robot.ssDriveTrain.arcadeDrive(Speed, -Robot.oi.driveController.getRawAxis(0));
-	    	//Robot.ssDriveTrain.arcadeDrive(Robot.oi.driveController.getRawAxis(0), Speed);
->>>>>>> 0e4bbdf5b6189c5bcaa3b762f0ee4db138f81889
-    	}
+		if(Robot.oi.driveControllerOn){
+	    	double Speed;
+	    	if(Robot.oi.driveController.getRawButton(4) == false){
+	    		Speed = Robot.oi.driveController.getRawAxis(3) - Robot.oi.driveController.getRawAxis(2);
+	    		Robot.ssDriveTrain.arcadeDrive(Speed, -Robot.oi.driveController.getRawAxis(0));
+	    	}
+	    	else{
+	    		Robot.ssDriveTrain.arcadeDrive((Robot.oi.driveController.getRawAxis(1)/2), (-Robot.oi.driveController.getRawAxis(0)/2));
+	    		//Robot.ssDriveTrain.arcadeDrive(Robot.oi.driveController.getRawAxis(0), Speed);
+	    	}
+		}
     }
 
     // Make this return true when this Command no longer needs to run execute()
