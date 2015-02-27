56ac18bc2a6eb7accac91ceff9d3d76439e305a3
Angelique Yang <angeliquenumber1@gmail.com>
2015-02-27 13:57:13 -0800

    added release brake

--- OhMars/src/org/usfirst/frc/team3663/robot/commands/C_ReleaseBrake.java
+++ OhMars/src/org/usfirst/frc/team3663/robot/commands/C_ReleaseBrake.java
@@ -29,11 +29,12 @@ public class C_ReleaseBrake extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
+    	Robot.ssElevator.bikeBrakeTriggerClose();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
-    	Robot.ssElevator.bikeBrakeTriggerClose();
+    	end();
     }
 }
